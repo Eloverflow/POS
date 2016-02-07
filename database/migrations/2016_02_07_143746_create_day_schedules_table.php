aa5752434d14c8c38cdcfeab0bd29f622bbc3f10
isaelblais <maype.isaelblais@gmail.com>
2016-02-07 16:40:48 -0500

    Avancement sur module Tracking employee, punch histories, fixed schedules list.

---
+++ database/migrations/2016_02_07_143746_create_day_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDaySchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('day_schedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('schedule_id')->unsigned();
+            $table->foreign('schedule_id')->references('id')->on('schedules');
+            $table->integer('employee_id')->unsigned();
+            $table->foreign('employee_id')->references('id')->on('employees');
+            $table->integer('day_number')->unsigned();
+            $table->time('startTime');
+            $table->time('endTime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('day_schedules');
+    }
+}
