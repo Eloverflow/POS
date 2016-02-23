25af53929cc743a9af0a47519726e5a058fda4a6
Jean <jeanfm93@hotmail.com>
2016-02-23 00:06:22 -0500

    choicelist centralize

--- resources/views/shared/create.blade.php
+++ resources/views/shared/create.blade.php
@@ -29,51 +29,7 @@
 
                         @if(isset($tableChoiceLists))
 
-                            <?php $tableIteration = 1; ?>
-
-                            @foreach($tableChoiceLists as $tableChoiceList)
-
-                                <label for="{{ $tableChoiceList['dbColumn'] }}">{{ $tableChoiceList["title"] }}</label>
-                                <input class="form-control input{{$tableIteration}}" type="hidden" id="{{ $tableChoiceList['dbColumn'] }}" name="{{ $tableChoiceList['dbColumn'] }}" value="">
-                                <div id="tableChoiceList{{$tableIteration}}" class="list-group tableChoiceList">
-                                    @foreach($tableChoiceList["table"] as $oneChoice)
-                                        <a id="{{$oneChoice->id}}" class="list-group-item tableChoice choiceList{{$tableIteration}} active' ">
-                                            @if($tableChoiceList["contentColumn"] != '' )
-                                                <h4 class="list-group-item-heading">{{ $oneChoice->$tableChoiceList["titleColumn"] }}</h4>
-                                                <p class="list-group-item-text">{{ $oneChoice->$tableChoiceList["contentColumn"] }}</p>
-                                            @else
-                                                <h2 class="list-group-item-heading">{{ $oneChoice->$tableChoiceList["titleColumn"] }}</h2>
-                                                <p class="list-group-item-text">{{ $oneChoice->created_at }}</p>
-                                            @endif
-
-                                        </a>
-                                    @endforeach
-                                        <a id="{{$oneChoice->id}}" class="list-group-item tableChoice focus choiceList{{$tableIteration}} active' ">
-
-                                                <div class="form-group choiceList{{$tableIteration}} add-new">
-
-                                                    <div class="block">
-                                                    <label class="control-label">{{ucwords( str_replace('_', ' ', $tableChoiceList["titleColumn"])) }} </label>
-                                                        <input id="newItemValue1" type="text" class="form-control choiceList{{$tableIteration}}" name="{{ $tableChoiceList["titleColumn"] }}" value="{{ old($tableChoiceList["titleColumn"]) }}"  disabled=true>
-                                                    </div>
-                                                    @if($tableChoiceList["contentColumn"] != "")
-                                                    <div class="block">
-                                                        <label  class="control-label">{{ ucwords( str_replace('_', ' ', $tableChoiceList["contentColumn"])) }}</label>
-                                                        <input  id="newItemValue2" type="text" class="form-control choiceList{{$tableIteration}}" name="{{ $tableChoiceList["contentColumn"] }}" value="{{old($tableChoiceList["contentColumn"])}}" disabled=true>
-                                                    </div>
-                                                    @endif
-
-                                                    <span id="{{ $tableChoiceList["postUrl"] }}" class="glyphicon glyphicon-ok-sign choiceList{{$tableIteration}}  sumbit-one action"></span>
-                                                    <span class="glyphicon glyphicon-remove-sign choiceList{{$tableIteration}}  cancel-one action"></span>
-                                                </div>
-                                            <span class="glyphicon glyphicon-plus-sign choiceList{{$tableIteration}}  add-one"></span>
-                                        </a>
-
-                                </div>
-                                <a><div id="tableChoiceListArrow{{$tableIteration}}" class="alert alert-info tableChoiceListArrow" role="alert"><span class="glyphicon glyphicon-chevron-down"></span></div></a>
-
-                                <?php $tableIteration++ ?>
-                            @endforeach
+                                @include('shared.choiceList')
                         @endif
 
                         <input type="hidden" name="_token" value="{{ csrf_token() }}" />
