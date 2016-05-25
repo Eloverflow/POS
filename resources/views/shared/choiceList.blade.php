97cdd4d8bd274de3083df3d0ccb8a4bb572f3fb7
isaelblais <maype.isaelblais@gmail.com>
2016-05-25 14:08:07 -0400

    commit seeder

--- resources/views/shared/choiceList.blade.php
+++ resources/views/shared/choiceList.blade.php
@@ -10,7 +10,7 @@
     <input class="form-control input{{$tableIteration}}" type="hidden" id="{{ $tableChoiceList['dbColumn'] }}" name="{{ $tableChoiceList['dbColumn'] }}" value="@if(isset($tableRow)){{ $tableRow->$tableChoiceList['dbColumn'] }} @endif">
     <div id="tableChoiceList{{$tableIteration}}" class="list-group tableChoiceList">
         @foreach($tableChoiceList["table"] as $oneChoice)
-            <span id="{{$oneChoice->id}}" data-field-names="{{$oneChoice->field_names}}" data-size-names="{{$oneChoice->size_names}}" class="list-group-item tableChoice choiceList{{$tableIteration}} active' @if(isset($tableRow) && $oneChoice->id == $tableRow->$tableChoiceList['dbColumn']) active @endif ">
+            <span id="{{$oneChoice->id}}" data-field-names="{{$oneChoice->field_names}}" data-size-names="{{$oneChoice->size_names}}" class="list-group-item tableChoice choiceList{{$tableIteration}} @if(isset($tableRow) && $oneChoice->id == $tableRow->$tableChoiceList['dbColumn']) active @endif ">
                 @if($tableChoiceList["contentColumn"] != '' )
                     <a class="view" href="{{ $tableChoiceList["postUrl"]}}/edit/{{$oneChoice->slug}}"><span class="glyphicon glyphicon-pencil"></span></a>
                     <h4 class="list-group-item-heading">{{ $oneChoice->$tableChoiceList["titleColumn"] }}</h4>
