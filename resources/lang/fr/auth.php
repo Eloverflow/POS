7170856ca1c6196c69664935158f39ec491e0699
eloverflow <jfortin-moreau@mirageflow.com>
2016-07-28 15:18:12 -0400

    lang fr integration

---
+++ resources/lang/fr/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'C\'est informations d\'authentification ne concorde pas avec nos enregistrements.',
+    'throttle' => 'Trop de tentative d\'authentification. S\'il-vous-plait veuillez reesayer dans :seconds secondes.',
+
+];
---
+++ resources/lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Le mot de passe doit etre au mois 6 caracteres de long et doit concorder avec la confirmation.',
+    'reset' => 'Votre mot de passe a ete reinitialize!',
+    'sent' => 'Nous vous avons envoyez un e-mail avec le lien de reinitialisation du mot de passe!',
+    'token' => 'Le token pour se mot de passe est invalide.',
+    'user' => "Impossible de trouver un usager avec cette addresse e-mail.",
+
+];
