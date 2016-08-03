8144b467acd11898843c63051e5d0e8ea348cedf
isaelblais <maype.isaelblais@gmail.com>
2016-08-03 12:44:06 -0400

    Commit first disponibility to availability try ...

--- resources/views/POS/Availability/create.blade.php
+++ resources/views/POS/Availability/create.blade.php
@@ -3,6 +3,10 @@
     <link rel="stylesheet" href="{{ @URL::to('css/fullcalendar.min.css') }}"/>
     <script src="{{ @URL::to('js/moment/moment.js') }}"></script>
     <script src="{{ @URL::to('js/moment/moment-timezone.js') }}"></script>
+
+    <script src="{{ @URL::to('Framework/Bootstrap/js/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ @URL::to('Framework/Bootstrap/js/bootstrap-datetimepicker.min.js') }}"></script>
+
     <script src="{{ @URL::to('js/fullcalendar.min.js') }}"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 @stop
@@ -18,6 +22,7 @@
         <div class="col-lg-12">
             <div class="panel panel-default">
                 <div class="panel-body">
+                    <div class="col-md-6">
                     {!! Form::open(array('url' => 'disponibility/create', 'role' => 'form', 'id' => 'frmDispoCreate')) !!}
 
                     <div id="displayErrors" style="display:none;" class="alert alert-danger">
@@ -57,6 +62,7 @@
                     </fieldset>
 
                     {!! Form::close() !!}
+                    </div>
                 </div>
             </div>
         </div>
@@ -80,47 +86,47 @@
             </div>
         </div>
     </div>
-    <div id="addModal" class="modal fade">
+    <div id="addModal" class="lumino modal fade">
         <div class="modal-dialog">
             <div class="modal-content">
                 <!-- dialog body -->
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Add Moment</h4>
+                </div>
+                <div class="row">
                     <div id="displayErrors" style="display:none;" class="alert alert-danger">
                         <strong>Whoops!</strong><br><br>
                         <ul id="errors"></ul>
                     </div>
-                    <div class="col-md-4">
-                        {!! Form::text('dateClicked', null, array('class' => 'form-control', 'id' => 'dateClicked', 'style' => 'display:none;visibility:hidden;')) !!}
+                    <div id="displaySuccesses" style="display:none;" class="alert alert-success">
+                        <strong>Success!</strong><div class="successMsg"></div>
+                    </div>
+                    <div class="col-md-6">
                         <div class="form-group">
                             <h3>Start Time</h3>
-                            <div class="col-md-6">
-                                {!! Form::label('sHour', "Hour" ) !!}
-                                {!! Form::text('sHour', old('sHour'), array('class' => 'form-control', 'id' => 'sHour')) !!}
-                            </div>
-                            <div class="col-md-6">
-                                {!! Form::label('sMin', "Min" ) !!}
-                                {!! Form::text('sMin', old('sMin'), array('class' => 'form-control', 'id' => 'sMin')) !!}
+                            <div class='input-group date' id="startTimePicker">
+                                <input type='text' id="startTime" class="form-control dark-border" />
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-time"></span>
+                                    </span>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-6">
                         <div class="form-group">
                             <h3>End Time</h3>
-                            <div class="col-md-6">
-                                {!! Form::label('eHour', "Hour" ) !!}
-                                {!! Form::text('eHour', old('eHour'), array('class' => 'form-control', 'id' => 'eHour')) !!}
-                            </div>
-                            <div class="col-md-6">
-                                {!! Form::label('eMin', "Min" ) !!}
-                                {!! Form::text('eMin', old('eMin'), array('class' => 'form-control', 'id' => 'eMin')) !!}
+                            <div class='input-group date' id="endTimePicker">
+                                <input type='text' id="startTime" class="form-control dark-border" />
+                                    <span class="input-group-addon">
+                                        <span class="glyphicon glyphicon-time"></span>
+                                    </span>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-6 pull-right">
                         <div class="form-group">
                             <h3>Day</h3>
-                            {!! Form::label('', "" ) !!}
                             <select id="dayNumber" class="form-control">
                                 <option value="-1">All Week</option>
                                 <option value="0">Sunday</option>
@@ -140,104 +146,43 @@
             </div>
         </div>
     </div>
-    <div id="editModal" class="modal fade">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <!-- dialog body -->
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <div id="displayErrors" style="display:none;" class="alert alert-danger">
-                        <strong>Whoops!</strong><br><br>
-                        <ul id="errors"></ul>
-                    </div>
-                    <div class="col-md-4">
-                        {!! Form::text('dateClicked', null, array('class' => 'form-control', 'id' => 'dateClicked', 'style' => 'display:none;visibility:hidden;')) !!}
-                        <div class="form-group">
-                            <h3>Start Time</h3>
-                            <div class="col-md-6">
-                                {!! Form::label('sHour', "Hour" ) !!}
-                                {!! Form::text('sHour', old('sHour'), array('class' => 'form-control', 'id' => 'sHour')) !!}
-                            </div>
-                            <div class="col-md-6">
-                                {!! Form::label('sMin', "Min" ) !!}
-                                {!! Form::text('sMin', old('sMin'), array('class' => 'form-control', 'id' => 'sMin')) !!}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <h3>End Time</h3>
-                            <div class="col-md-6">
-                                {!! Form::label('eHour', "Hour" ) !!}
-                                {!! Form::text('eHour', old('eHour'), array('class' => 'form-control', 'id' => 'eHour')) !!}
-                            </div>
-                            <div class="col-md-6">
-                                {!! Form::label('eMin', "Min" ) !!}
-                                {!! Form::text('eMin', old('eMin'), array('class' => 'form-control', 'id' => 'eMin')) !!}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="form-group">
-                            <h3>Day</h3>
-                            {!! Form::label('', "" ) !!}
-                            <select id="dayNumber" class="form-control">
-                                <option value="0">Sunday</option>
-                                <option value="1">Monday</option>
-                                <option value="2">Tuesday</option>
-                                <option value="3">Wednesday</option>
-                                <option value="4">Thursday</option>
-                                <option value="5">Friday</option>
-                                <option value="6">Saturday</option>
-                            </select>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- dialog buttons -->
-                <div class="modal-footer">
-                    <button id="btnDelEvent" type="button" class="btn btn-danger">Delete</button>
-                    <button id="btnEditEvent" type="button" class="btn btn-primary">Edit</button>
-                </div>
-            </div>
-        </div>
-    </div>
 @stop
 
 @section("myjsfile")
     <script src="{{ @URL::to('js/utils.js') }}"></script>
     <script src="{{ @URL::to('js/availabilityManage.js') }}"></script>
+
+    <script src="{{ @URL::to('js/moment/moment-timezone-with-data-packed.js') }}"></script>
     <script type="text/javascript">
         // var for edit Event
         var globStoredEvent = null;
         var globStoredCalendar = $('#calendar-' + "{{$ViewBag['calendar']->getId() }}");
-
         $('#btnAdd').click(function(e) {
-            $('#addModal #sHour').val("");
-            $('#addModal #sMin').val("");
 
-            $('#addModal #eHour').val("");
-            $('#addModal #eMin').val("");
-
-            var lastSunday = getLastSunday(new Date());
-            $('#addModal #dateClicked').val(formatDate(lastSunday));
             $("#addModal").modal('show');
         });
         $('#btnFinish').click(function(e) {
             e.preventDefault();
-            postAddDisponibilities(globStoredCalendar);
+            postAddDisponibilities();
 
         });
         $("#btnDelEvent").click(function(){
-            deleteEvent(globStoredCalendar);
+            deleteEvent();
             $("#editModal").modal('hide');
         });
         $("#btnEditEvent").click(function(){
-            editEvent(globStoredCalendar);
+            editEvent();
         });
         $("#btnAddEvent").click(function() {
-            addEvent(globStoredCalendar);
+            addEvent();
         });
+
+        $('#addModal #startTimePicker').datetimepicker();
+        $('#addModal #endTimePicker').datetimepicker();
+
+        $('#editModal #startTimePicker').datetimepicker();
+        $('#editModal #endTimePicker').datetimepicker();
+
         $( "#dayNumber" ).change(function() {
             //var nDate = new Date();
             //nDate.setDate(nDate.getFullYear() + "-" +  nDate.getMonth() + "-" + (nDate.getDate() + this.value));
