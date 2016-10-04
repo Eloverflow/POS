1288428a9593b2fe00b09247d1ea9a376906fe44
eloverflow <jfortin-moreau@mirageflow.com>
2016-10-04 18:44:21 -0400

    new mysql 5.7 install

--- .travis_install-mysql-5.7.sh
+++ .travis_install-mysql-5.7.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
-wget http://dev.mysql.com/get/mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
-sudo dpkg --install mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
-sudo apt-get update -q
-sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
+export DEBIAN_FRONTEND=noninteractive
+
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-codename select trusty'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-distro select ubuntu'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/repo-url string http://repo.mysql.com/apt/'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-preview select '
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-product select Ok'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-server select mysql-5.7'
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/select-tools select '
+debconf-set-selections <<< 'mysql-apt-config mysql-apt-config/unsupported-platform select abort'
+
+wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
+dpkg -i mysql-apt-config_0.7.3-1_all.deb
+apt-get update
+apt-get install -y mysql-server-5.7
