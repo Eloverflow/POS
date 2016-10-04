ba2aa286630a1375d5a5221531341535d497e137
eloverflow <jfortin-moreau@mirageflow.com>
2016-10-04 16:16:38 -0400

    return of DB env

--- .travis_create_db.sh
+++ .travis_create_db.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-mysql --execute "CREATE DATABASE IF NOT EXISTS \`POS\`;" --user=root
-mysql --execute "USE \`POS\`;" --user=root
-mysql --execute "CREATE USER 'POS'@'localhost';" --user=root
-mysql --execute "GRANT ALL PRIVILEGES ON \`POS\`.* TO 'POS'@'localhost';" --user=root
-mysql --execute "FLUSH PRIVILEGES;" --user=root
+echo "DB=$DB"
+
+if [[ "$DB" == "mysql" ]]; then
+  mysql --execute "CREATE DATABASE IF NOT EXISTS \`POS\`;" --user=root
+  mysql --execute "USE \`POS\`;" --user=root
+  mysql --execute "CREATE USER 'POS'@'localhost';" --user=root
+  mysql --execute "GRANT ALL PRIVILEGES ON \`POS\`.* TO 'POS'@'localhost';" --user=root
+  mysql --execute "FLUSH PRIVILEGES;" --user=root
+fi
