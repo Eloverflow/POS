6ae03a904ecc5c4e3120549c2913c2e33a2c2f25
eloverflow <jfortin-moreau@mirageflow.com>
2016-10-04 16:06:23 -0400

    no condition on database POS create for testing + app name and version

--- .travis_create_db.sh
+++ .travis_create_db.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-echo "DB=$DB"
-
-if [[ "$DB" == "mysql" ]]; then
-  mysql --execute "CREATE DATABASE IF NOT EXISTS \`POS\`;" --user=root
-  mysql --execute "USE \`POS\`;" --user=root
-  mysql --execute "CREATE USER 'POS'@'localhost';" --user=root
-  mysql --execute "GRANT ALL PRIVILEGES ON \`POS\`.* TO 'POS'@'localhost';" --user=root
-  mysql --execute "FLUSH PRIVILEGES;" --user=root
-fi
+mysql --execute "CREATE DATABASE IF NOT EXISTS \`POS\`;" --user=root
+mysql --execute "USE \`POS\`;" --user=root
+mysql --execute "CREATE USER 'POS'@'localhost';" --user=root
+mysql --execute "GRANT ALL PRIVILEGES ON \`POS\`.* TO 'POS'@'localhost';" --user=root
+mysql --execute "FLUSH PRIVILEGES;" --user=root
