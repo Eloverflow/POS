ccce5a223d7183e00612fdff20c99d7562ab2162
eloverflow <jfortin-moreau@mirageflow.com>
2016-10-10 14:03:14 -0400

    work titles

--- tests/WorkTitlesTest.php
+++ tests/WorkTitlesTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class EmployeesTest extends TestCase
+class WorkTitlesTest extends TestCase
 {
 
     public function testCreateWorkTitle()
