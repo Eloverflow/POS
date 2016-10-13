7c801f0eff419a5b5fa3ae401d6a96e1381ef74f
eloverflow <jfortin-moreau@mirageflow.com>
2016-10-13 13:58:32 -0400

    selenium webdriver for user manipulation test

--- .travis_install-mysql-5.7_Selenium.sh
+++ .travis_install-mysql-5.7_Selenium.sh
@@ -16,3 +16,7 @@ sudo apt-get install -y mysql-community-server
 #sudo mysqld_safe --skip-grant-tables &>/dev/null &
 sudo mysql -e "USE mysql;CREATE USER 'posio'@'localhost' IDENTIFIED BY 'superstringpassword';GRANT ALL PRIVILEGES ON *.* TO 'posio'@'localhost';flush privileges;" --user=root
 sudo /etc/init.d/mysql restart
+
+wget http://selenium-release.storage.googleapis.com/3.0-beta4/selenium-java-3.0.0-beta4.zip
+unzip selenium-java-3.0.0-beta4.zip /usr/local/bin
+java -jar /usr/local/bin/client-combined-3.0.0-beta4-nodeps.jar
---
+++ tests/POSMenuTestSelenium.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class WebPOSMenuTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl('http://www.example.com/');
+    }
+
+    public function testTitle()
+    {
+        $this->url('http://www.example.com/');
+        $this->assertEquals('Example WWW Page', $this->title());
+    }
+/*
+
+    public function testPagePOSMenu()
+    {
+        $user = factory(App\Models\Auth\User::class)->create();
+
+        $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->visit('/menu');
+
+        sleep(10); // Give the time to Angular for loading
+
+        $this->click('btn-menu-3')
+            ->click('btn-menu-enter')
+            ->click('btn-menu-1')
+            ->click('btn-menu-1')
+            ->click('btn-menu-enter');
+
+
+        sleep(3); // Give the time to Angular for loading
+
+        $this->see('Commande - Client: #1');
+    }
+
+    public function testPagePOSMenuPunch()
+    {
+        $user = factory(App\Models\Auth\User::class)->create();
+
+        $this->actingAs($user)
+            ->withSession(['foo' => 'bar'])
+            ->visit('/menu');
+
+        sleep(10); // Give the time to Angular for loading
+
+        $this->click('btn-menu-3')
+            ->click('btn-menu-clk');
+
+        sleep(3); // Give the time to Angular for loading
+
+        $this->click('btn-Barmaid')
+            ->see('The employee has been successfully punched in !')
+            ->click('btn-menu-clk')
+            ->see('The employee has been successfully punched out !');
+    }
+*/
+
+}
