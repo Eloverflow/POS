d98caae6714562c0f31cb5ad72af47b9387ac0de
eloverflow <jfortin-moreau@mirageflow.com>
2016-10-13 14:31:28 -0400

    selenium webdriver for user manipulation test

--- .travil_install-selenium.sh
+++ .travil_install-selenium.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt-get install -y default-jre unzip
 wget http://selenium-release.storage.googleapis.com/3.0-beta4/selenium-java-3.0.0-beta4.zip
 unzip selenium-java-3.0.0-beta4.zip /usr/local/bin
