b1366e4c105d417e6bcc4987c16e7ff583b5c4df
eloverflow <jfortin-moreau@mirageflow.com>
2016-10-13 14:57:56 -0400

    selenium webdriver for user manipulation test

--- .travis_install-selenium.sh
+++ .travis_install-selenium.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-get install -y default-jre unzip
 wget https://goo.gl/LSKE9I
+sudo chmod 777 selenium-server-standalone-3.0.0.jar
 sudo java -jar selenium-server-standalone-3.0.0.jar
