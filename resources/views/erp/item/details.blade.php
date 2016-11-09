6890e04a8586dacf94205afef6711cf5a7e0843b
eloverflow <jfortin-moreau@mirageflow.com>
2016-11-09 11:47:14 -0500

    item details

--- resources/views/erp/item/details.blade.php
+++ resources/views/erp/item/details.blade.php
@@ -22,21 +22,32 @@
             <div class="panel panel-default">
                 <div class="panel-body">
                     <div class="col-md-12">
-                        {!! Form::open(array('url' => 'inventory/update', 'role' => 'form')) !!}
-
-
                         <div class="form-group">
-                            <label for="item" >Item</label>
-                            <p>{{ $items }}</p>
-                            <label for="item" >Item Size</label>
+                            <label for="item" >Name</label>
+                            <p>{{ $items->name }}</p>
+                            <label for="item" >Description</label>
+                            <p>{{ $items->description }}</p>
+
+                            <label for="item" >Custom Fields</label>
+                            <?php $fields = json_decode($items->custom_fields_array); $field_names = explode(',',$items->itemtype->field_names)?>
+                            @for($i = 0; $i < count($field_names); $i++)
+                                <p>{{ $field_names[$i] }} : {{ $fields[$i] }}</p>
+                            @endfor
+
+                            <label for="item" >Size prices</label>
+                            <?php $sizes_price = json_decode($items->size_prices_array); $size_names = explode(',',$items->itemtype->size_names)?>
+                            @for($i = 0; $i < count($size_names); $i++)
+                                <p>{{ $size_names[$i] }} : {{ $sizes_price[$i] }}</p>
+                            @endfor
+
+                            @if(!empty($items->img_id))
+                                <label for="item" >Image</label>
+                                <div class="">
+                                    <img id="img_display" src="{{ @URL::to('img/item/' . $items->img_id) }}" alt=""  width="300">
+                                </div>
+                            @endif
+
                         </div>
-
-
-                        <div class="form-group">
-                            <label for="quantity" >Quantité</label>
-                        </div>
-
-
                     </div>
                 </div>
 
