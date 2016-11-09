39eec440b231de9e4fd2c6f8651fa5a3dae44e11
eloverflow <jfortin-moreau@mirageflow.com>
2016-11-09 11:14:15 -0500

    plan preview

--- resources/views/POS/Plan/details.blade.php
+++ resources/views/POS/Plan/details.blade.php
@@ -3,66 +3,42 @@
 @section('content')
     <div class="row">
         <div class="col-lg-12">
-            <h1 class="page-header">Employee Details</h1>
+            <h1 class="page-header">Plan Preview</h1>
         </div>
     </div>
-    <div class="row">
+    <div class="row" ng-app="myApp" >
         <div class="col-lg-12">
             <div class="panel panel-default">
-                <div class="panel-body">
-
-                    <div class="form-group">
-                        <label>Email :</label>
-                        <p>{{ $employee->email }}</p>
-                    </div>
-
-                    <div class="form-group">
-                        <label>First Name :</label>
-                        <p>{{ $employee->firstName }}</p>
-                    </div>
-                    <div class="form-group">
-                        <label>Last Name :</label>
-                        <p>{{ $employee->lastName }}</p>
-                    </div>
-                    <div class="form-group">
-                        <label>Address :</label>
-                        <p>{{ $employee->streetAddress }}</p>
-                    </div>
-                    <div class="form-group">
-                        <label>City :</label>
-                        <p>{{ $employee->city }}</p>
-                    </div>
-                    <div class="form-group">
-                        <label>State :</label>
-                        <p>{{ $employee->state }}</p>
-                    </div>
-                    <div class="form-group">
-                        <label>Phone Number :</label>
-                        <p>{{ $employee->phone }}</p>
-                    </div>
-                    <div class="form-group">
-                        <label>Postal Code :</label>
-                        <p>{{ $employee->pc }}</p>
-                    </div>
-                    <div class="form-group">
-                        <label>NAS :</label>
-                        <p>{{ $employee->nas }}</p>
-                    </div>
-
-                    <div class="form-group">
-                        <label>Bonus Salary :</label>
-                        <p>{{ $employee->bonusSalary }}</p>
-                    </div>
-                    <div class="form-group">
-                        <label>Birth Date :</label>
-                        <p>{{ $employee->birthDate }}</p>
-                    </div>
-                    <div class="form-group">
-                        <label>Hire Date :</label>
-                        <p>{{ $employee->hireDate }}</p>
+                <div class="panel-body" ng-controller="menuController">
+                    <div id="planModal">
+                        <span id="floorNumber" >Étage #<% plan.currentFloor+1 %></span>
+                        <div class="planRightButton">
+                            <span id="planZoomout" class="glyphicon glyphicon-zoom-out"></span>
+                            <span ng-show="plan.currentFloor < plan.nbFloor-1"  ng-click="floorUp()"  id="floorup" class="glyphicon glyphicon-upload"></span>
+                            <span ng-show="plan.currentFloor > 0" ng-click="floorDown()" id="floordown" class="glyphicon glyphicon-download"></span>
+                        </div>
+                        <div class="parent">
+                            <div class="panzoom">
+                                <canvas style="margin: 0;" id="planCanvas" width="0" height="0" />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @stop
+
+
+@section('myjsfile')
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.8/angular.min.js"></script>
+    <script src="{{ @URL::to('js/jquery.panzoom.min.js') }}"></script>
+    <script src="{{ @URL::to('js/posMenuPlanPreview.js') }}"></script>
+    <script >
+        $('#sidebar-collapse').delay(50).animate({width: 0}, 200);
+        $('.col-sm-9.col-sm-offset-3.col-lg-10.col-lg-offset-2').delay(50).animate({width: '100%', marginLeft: 0, position: 'absolute'}, 200)
+
+    </script>
+
+@stop
+
